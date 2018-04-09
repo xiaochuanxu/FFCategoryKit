dc6ec961ce6d07d9594f30c0fdfcd62179ea9947
徐晓川 <xuxiaochuan@zbj.com>
2018-04-09 16:24:59 +0800

    Initial commit

---
+++ Example/FFCategoryKit/XXCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XXCAppDelegate.h
+//  FFCategoryKit
+//
+//  Created by xiaochuanxu on 04/09/2018.
+//  Copyright (c) 2018 xiaochuanxu. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XXCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/FFCategoryKit/XXCViewController.m
@@ -0,0 +1,29 @@
+//
+//  XXCViewController.m
+//  FFCategoryKit
+//
+//  Created by xiaochuanxu on 04/09/2018.
+//  Copyright (c) 2018 xiaochuanxu. All rights reserved.
+//
+
+#import "XXCViewController.h"
+
+@interface XXCViewController ()
+
+@end
+
+@implementation XXCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
